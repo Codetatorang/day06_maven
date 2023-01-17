53932b56df4c07f024f5d880cd6cb97da4d7d17d
Vincent Ang <vinceajy93@gmail.com>
2023-01-17 11:31:33 +0800

    Threading using executable service

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss;
 
+import java.lang.reflect.Executable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -22,13 +26,42 @@ public final class App {
             }
 
         });
-        thread1.start();
+        // thread1.start();
 
-        MyRunnableImplementation mRI = new MyRunnableImplementation();
-        Thread thread2 = new Thread(mRI);
-        thread2.start();
+        MyRunnableImplementation mRI = new MyRunnableImplementation("task 1");
+        MyRunnableImplementation mRI2 = new MyRunnableImplementation("task 2");
+        MyRunnableImplementation mRI3 = new MyRunnableImplementation("task 3");
+        MyRunnableImplementation mRI4 = new MyRunnableImplementation("task 4");
+        MyRunnableImplementation mRI5 = new MyRunnableImplementation("task 5");
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
 
-        Thread thread3 = new Thread(mRI);
-        thread3.start();
+        // Thread thread3 = new Thread(mRI);
+        // thread3.start();
+
+        //single thread exercuteable
+        // ExecutorService exercutorService = Executors.newSingleThreadExecutor();
+        // exercutorService.execute(mRI);
+        // exercutorService.execute(mRI2);
+        // exercutorService.shutdown();
+
+        // ExecutorService exercutorService = Executors.newFixedThreadPool(3);
+        // exercutorService.execute(mRI);
+        // exercutorService.execute(mRI2);
+        // exercutorService.execute(mRI3);
+        // exercutorService.execute(mRI4);
+        // exercutorService.execute(mRI5);
+        // exercutorService.shutdown();
+        
+        ExecutorService exercutorService = Executors.newCachedThreadPool();
+        exercutorService.execute(mRI);
+        exercutorService.execute(mRI2);
+        exercutorService.execute(mRI3);
+        exercutorService.execute(mRI4);
+        exercutorService.execute(mRI5);
+        exercutorService.shutdown();
+        
+        
+
     }
 }
--- MyRunnableImplementation.java
+++ MyRunnableImplementation.java
@@ -14,7 +14,8 @@ public class MyRunnableImplementation implements Runnable {
     @Override
     public void run(){
         for(int i =0; i < 5; i++){
-            System.out.println(Thread.currentThread().getName() + "\tRunnable..." + i);
+            System.out.println(Thread.currentThread().getName() + "\t" + taskName +
+            "\tRunnable..." + i);
         }
     }
 }
